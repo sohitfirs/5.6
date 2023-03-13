9f61fc4c00deec128d6ebec766748d639f106b60
sohitfirs <dokukin.i@mail.ru>
2023-03-13 11:02:03 +0300

    5.6

---
+++ main.cpp
@@ -0,0 +1,63 @@
+/* https://github.com/sohitfirs/5.6.git */
+
+#include <stdio.h>
+#include <cstdlib>
+#include <time.h>
+
+main()
+{
+	int mas[100][100];
+	int n, m;
+	
+	printf("n=");
+	scanf("%i", &n);
+	
+	printf("m=");
+	scanf("%i", &m);
+	
+	srand(time(NULL));
+	
+	for (int i=0; i<n; i++)
+	{
+		for (int j=0; j<m; j++)
+		{
+			mas[i][j]=rand()%11;
+		}
+	}
+	
+	for (int i=0; i<n; i++)
+	{
+		for (int j=0; j<m; j++)
+		{
+			printf("%3i", mas[i][j]);
+		}
+		printf("\n");
+	}
+
+	for(int j=0; j<m/2; j++)
+	{
+		for(int i=0; i<n; i++) 
+		{
+			if(mas[i][j]==mas[i][0] || mas[i][j]==mas[i][m-1])
+			{
+				int q = mas[i][j];
+				mas[i][j] = mas[i][m-1-j];
+				mas[i][m-1-j] = q;
+			}
+		}
+	}
+	
+	printf("\n\n\n\n");
+	
+	for (int i=0; i<n; i++)
+	{
+		for (int j=0; j<m; j++)
+		{
+			printf("%3i", mas[i][j]);
+		}
+		printf("\n");
+	}
+	
+	system("pause");
+}
+
